8fbf3578a2037d0e6568f5e451fe4aa9bda04838
Bagrat Aznauryan <bagrat@aznauryan.org>
2015-05-11 20:50:24 +0400

    Improve PyPI extension rollbacks

--- pylease/extension/pypi.py
+++ pylease/extension/pypi.py
@@ -1,4 +1,5 @@
 import sys
+from pylease.command.rollback import Rollback, Stage
 from pylease.logger import LOGME as logme  # noqa
 from pylease.command.task import AfterTask
 from pylease.extension import Extension
@@ -17,19 +18,34 @@ class PypiExtension(Extension):
                                 default=False,
                                 help='Upload package to PyPI')
 
-        make_command.add_after_task(PypiAfterTask(rollback))
+        make_command.add_after_task(PypiAfterTask())
 
 
 class PypiAfterTask(AfterTask):  # pragma: no cover - Unable to test this other than manually TODO: try to test
     def execute(self, lizy, args):
         if args.to_pypi:
-            self.enable_rollback()
+            self.enable_rollback(PypiRollback(sys.argv))
 
-            orig_args = sys.argv
             sys.argv = ['setup.py', 'sdist', 'upload']
+            self._rollback.enable_stage(PypiRollback.UPLOADING_STAGE)
+
             __import__('setup')
-            sys.argv = orig_args
+            self._rollback.enable_stage(PypiRollback.UPLOADED_STAGE)
 
 
-def rollback():  # pragma: no cover
-    logme.debug('Unable to rollback PyPI upload, please delete the uploaded version manually.')
+class PypiRollback(Rollback):  # pragma: no cover
+    UPLOADED_STAGE = 'uploaded'
+    UPLOADING_STAGE = 'uploading'
+
+    def __init__(self, orig_args):
+        super(PypiRollback, self).__init__()
+
+        self._orig_args = orig_args
+
+    @Stage(UPLOADING_STAGE)
+    def rollback_args(self):
+        sys.argv = self._orig_args
+
+    @Stage(UPLOADED_STAGE)
+    def rollback_inform(self):
+        logme.warn('Unable to rollback PyPI upload, please delete the uploaded version manually.')
